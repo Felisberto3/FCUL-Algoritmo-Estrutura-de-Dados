404bd17e8d4fc8ec65b50ca3f6ca2fe214277abc
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-03-22 14:22:42 +0000

    JAVADOC no caso 1 da serie 4

--- Series04/ConjuntoDeInteiros.java
+++ Series04/ConjuntoDeInteiros.java
@@ -3,13 +3,28 @@ package Series04;
 import java.util.Arrays;
 
 public class ConjuntoDeInteiros {
+    /**
+     * cria um conjunto de numeros inteiros
+     */
     private int[] conjunto;
 
+    /**
+     * copia um array de inteiros na propriedade conjunto e organiza em ordem
+     * crescente
+     * 
+     * @param a conjunto de numeros inteiros
+     */
     public ConjuntoDeInteiros(int[] a) {
         conjunto = Arrays.copyOf(a, a.length);
         Arrays.sort(conjunto);
     }
 
+    /**
+     * Verifica se um elemento faz parte do conjunto
+     * 
+     * @param elemento a ser pesquisado no conjunto
+     * @return True se o elemento estiver no conjunto e false caso contrario
+     */
     public boolean contem(int elemento) {
         return Arrays.binarySearch(conjunto, elemento) >= 0;
     }
--- Series04/Financas.java
+++ Series04/Financas.java
@@ -2,6 +2,12 @@ package Series04;
 
 public class Financas {
 
+    /**
+     * Mostra a lista de clientes que não são devedores
+     * 
+     * @param devedores é a lista dos devedores
+     * @param clientes  é a lista dos clientes = (devedores como não devedores)
+     */
     public void listaLivre(int[] devedores, int[] clientes) {
         ConjuntoDeInteiros zDevedores = new ConjuntoDeInteiros(devedores);
 
@@ -14,8 +20,8 @@ public class Financas {
 
     public static void main(String[] args) {
         int[] devedores = { 142537498, 176483908, 210023492, 198361023 };
-
         int[] clientes = { 219826341, 210023492, 187232409, 198361023, 198436129 };
+
         Financas financas = new Financas();
         financas.listaLivre(devedores, clientes);
 
