60cd9636d21bbed5ec5034994e0b1ecff50505c6
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-03-22 19:34:37 +0000

    data e dataRadical

--- Series04/Data.java
+++ Series04/Data.java
@@ -1,5 +1,14 @@
 package Series04;
 
+/**
+ * Pré Condição:
+ * Dia deve ser [1..31],
+ * -------se mes {4,6,9,11} pertence [1..31], se mes = 2 pertence
+ * -------[1..29] no ano bisexto e [1..28] caso contrário.
+ * Mes pertence [1..12]
+ * Ano >= 0.
+ * Método auxiliar: isValidData(dia, mes, ano)
+ */
 public class Data {
     /**
      * Cria uma data de acordo ao parametro informado
@@ -9,19 +18,33 @@ public class Data {
     private int ano;
 
     /**
-     * Cria a data no momento da inicialização.
      * 
-     * @param dia da data em questão.
-     * @param mes da data em questão.
-     * @param ano da data em questão.
-     * @requires Dia deve ser maior que 0 e inferior a 32.
-     * @requires Mes deve ser maior que 0 e inferior a 13.
-     * @requires Ano deve ser maior que 0.
+     * @param dia
+     * @param mes
+     * @param ano
      */
     public Data(int dia, int mes, int ano) {
+        if (!isValidData(dia, mes, ano)) {
+            throw new IllegalArgumentException("Data não Válida!");
+        }
         this.dia = dia;
         this.mes = mes;
         this.ano = ano;
+    }
+
+    private boolean isValidData(int dia, int mes, int ano) {
+        switch (mes) {
+            case 1, 2, 3, 5, 7, 8, 10, 12:
+                return dia >= 1 && dia <= 31;
+            case 4, 6, 9, 11:
+                return dia >= 1 && dia <= 30;
+            default:
+                return dia >= 1 && dia <= (isBisexto(ano) ? 29 : 28);
+        }
+    }
+
+    public boolean isBisexto(int ano) {
+        return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
     }
 
     /**
---
+++ Series04/DataRadical.java
@@ -0,0 +1,97 @@
+package Series04;
+
+/**
+ * Representa a data utilizando um único inteiro.
+ */
+public class DataRadical {
+
+    private int data;
+
+    /**
+     * Pré condicao.
+     * 
+     * @params dia pertence ao [1..31];
+     *         mes pertence {4,6,9,11} entao [1..30];
+     *         se mes= 2 entao [1..29]
+     * @params 1<= mes <=12,
+     * @params ano >= 0.
+     * 
+     *         Pós condição
+     *         O objecto data é usado com os dados fornecidos assumindo que são
+     *         válidos.
+     * 
+     */
+    public DataRadical(int dia, int mes, int ano) {
+        if (!isValidData(dia, mes, ano)) {
+            throw new IllegalArgumentException("Data não Válida!");
+        }
+        this.data = 512 * ano + mes * 32 + dia;
+    }
+
+    /**
+     * 
+     * @return dia da data específica.
+     */
+    public int dia() {
+        return data % 32;
+    }
+
+    /**
+     * 
+     * @return mes da data específica.
+     */
+    public int mes() {
+        return (data % 512) / 32;
+    }
+
+    /**
+     * 
+     * @return ano da data específica.
+     */
+    public int ano() {
+        return data / 512;
+    }
+
+    /**
+     * 
+     * @param dia verifica se o dia e válido.
+     * @param mes verifica se o mes é válido
+     * @param ano verifica se o ano é válido.
+     * @return true se a data é válida, false caso contrário.
+     */
+    private boolean isValidData(int dia, int mes, int ano) {
+
+        switch (mes) {
+            case 4, 6, 9, 11:
+                return dia >= 1 && dia <= 30;
+            case 2:
+                return dia >= 1 && dia <= (isBisexto(ano) ? 29 : 28);
+            case 1, 3, 5, 7, 8, 10, 12:
+                return dia >= 1 && dia <= 31;
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * 
+     * @param ano
+     * @return true se for bisexto e false caso contrário.
+     */
+    private boolean isBisexto(int ano) {
+        return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
+    }
+
+    /**
+     * @return a data de forma dd/mm/aa
+     */
+    public String toString() {
+        return dia() + "/" + mes() + "/" + ano();
+    }
+
+    public static void main(String[] args) {
+        DataRadical data = new DataRadical(29, 03, 2028);
+        System.out.println(data);
+    }
+
+}
