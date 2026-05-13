78add8c2aa4bd514505b0f658621f7fc4dd7def3
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-05-13 21:10:06 +0100

    HeapSort

---
+++ Teoria/Sort/HeapSort/HeapClass.java
@@ -0,0 +1,47 @@
+package Teoria.Sort.HeapSort;
+
+public class HeapClass {
+    public static void main(String[] args) {
+        int[] a = { 0, 1, 60, 50, 8, 20, 100 };
+        sort(a);
+
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void sort(int[] a) {
+        int n = a.length - 1;
+        for (int k = n / 2; k >= 1; k--) {
+            heapify(a, k, n);
+        }
+
+        while (n > 1) {
+            exch(a, 1, n--);
+            heapify(a, 1, n);
+        }
+    }
+
+    public static void heapify(int[] a, int k, int n) {
+        while (2 * k <= n) {
+            int j = 2 * k;
+            if (j < n && less(a, j, j + 1))
+                j++;
+            if (!less(a, k, j))
+                break;
+            exch(a, k, j);
+            k = j;
+
+        }
+    }
+
+    public static void exch(int[] a, int k, int j) {
+        int aux = a[k];
+        a[k] = a[j];
+        a[j] = aux;
+    }
+
+    public static boolean less(int[] a, int i, int j) {
+        return a[i] < a[j];
+    }
+}
